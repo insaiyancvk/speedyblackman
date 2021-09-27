2446ad3da421b3c617a290b9afef4850c6f0ad68
insaiyancvk <cvamshik1@gmail.com>
2021-09-27 19:55:34 +0530

    add multithreading for capturing the frames

--- collector.py
+++ collector.py
@@ -1,10 +1,79 @@
 # For collecting the data
-import keyboard, pyscreenshot, uuid, os
-from datetime import datetime
+import keyboard, pyscreenshot, uuid, os, time, threading
+from hook import Hook, KeyboardEvent
 
 
-#           !!!!!!!!!!!  IMPORTANT  !!!!!!!!!!!!
-#           PLAY THE GAME IN FIRST PERSON PERSPECTIVE WITH BONET VISIBLE
+'''           !!!!!!!!!!!  IMPORTANT  !!!!!!!!!!!!
+              PLAY THE GAME IN FIRST PERSON PERSPECTIVE WITH BONET VISIBLE      '''
+
+class set1():
+
+    def __init__(self):
+
+        self.set1_dirs()
+        hk = Hook()
+        hk.handler = self.set1_events
+        thread = threading.Thread(target=hk.hook)
+        thread.start()
+
+    def set1_dirs(self):
+        ''' Function that takes care of capturing the frames when acceleration or break is used '''
+
+        print("checking for set1, w, s and space directories")
+        
+        if not (os.path.isdir("./set1") and os.path.isdir("./set1/w") and os.path.isdir("./set1/s") and os.path.isdir("./set1/space")):
+            
+            try:
+                os.mkdir("./set1")
+                print("set1 directory not found, creating one")
+            except: pass
+
+            try:
+                os.mkdir("./set1/w")
+                print("w directory not found, creating one")
+            except: pass
+
+            try:
+                os.mkdir("./set1/s")
+                print("s directory not found, creating one")
+            except: pass
+
+            try:
+                os.mkdir("./set1/space")
+                print("space directory not found, creating one")
+            except: pass
+
+    def set1_events(self, args):
+        
+        if isinstance(args, KeyboardEvent):
+            print(args.key_code)
+            if 'W' in args.pressed_key:
+                pyscreenshot.grab(bbox=(0, 155, 1920, 1080)).save(f"./set1/w/{uuid.uuid1().hex}.png")
+                print(f"Saved W set1/w/{uuid.uuid1().hex}.png")
+            
+            elif 'S' in args.pressed_key:
+                pyscreenshot.grab(bbox=(0, 155, 1920, 1080)).save(f"./set1/s/{uuid.uuid1().hex}.png")
+                print(f"Saved S set1/s/{uuid.uuid1().hex}.png")
+
+            elif 'Space' in args.pressed_key:
+                pyscreenshot.grab(bbox=(0, 155, 1920, 1080)).save(f"./set1/space/{uuid.uuid1().hex}.png")
+                print("Saved space capture")
+
+
+if __name__ == '__main__':
+
+    print("Checking for \"data\" directory")
+
+    try:
+        os.chdir("./data/")
+        print("\"data\" directory found")
+    except:
+        print("\"data\" directory not found, creating one")
+        os.mkdir("./data/")
+        os.chdir("./data")
+
+    set1()
+
 
 def capture_screen(name, cord):
     '''
@@ -17,70 +86,28 @@ def capture_screen(name, cord):
     image = pyscreenshot.grab(bbox=cord)
     # print(cord)
     image.save(name)
-    print(f"{name} saved at {datetime.now().time()}")
+    print(f"{name} saved")
 
-print("Checking for \"data\" directory")
-try:
-    os.chdir("./data/")
-    print("\"data\" directory found")
-except:
-    print("\"data\" directory not found, creating one")
-    os.mkdir("./data/")
-    os.chdir("./data")
-
-# Function that takes care of capturing the frames when acceleration or break is used
-def capture_set1():
-
-    print("checking for set1, w, s and space directories")
-    # try:
-    #     os.chdir("./set1")
-    # except:
-    #     os.mkdir("./set1")
-    #     os.chdir("./set1")
+def set1_events(args):
     
-    if not (os.path.isdir("./set1") and os.path.isdir("./set1/w") and os.path.isdir("./set1/s") and os.path.isdir("./set1/space")):
+    if 'W' in args.pressed_key:
+        start = time.time()
+        pyscreenshot.grab(bbox=(0, 155, 1920, 1080)).save(f"./set1/{(args.pressed_key).lower}/{uuid.uuid1().hex}.png")
+        time.sleep(0.6)
+        # capture_screen(, cord)
+        print(f"{time.time()-start}s")
+        # print("INSIDE W")
         
-        try:
-            os.mkdir("./set1")
-            print("set1 directory not found, creating one")
-        except:
-            pass
+    elif 'S' in args.pressed_key:
+        print("INSIDE S")
+        capture_screen(f"./set1/{(args.pressed_key).lower}/{uuid.uuid1().hex}.png", cord=(0, 155, 1920, 1080))
 
-        try:
-            os.mkdir("./set1/w")
-            print("w directory not found, creating one")
-        except:
-            pass
+    elif 'Space' in args.pressed_key:
+        print("INSIDE SPACE")
+        capture_screen(f"./set1/{(args.pressed_key).lower}/{uuid.uuid1().hex}.png", cord=(0, 155, 1920, 1080))
 
-        try:
-            os.mkdir("./set1/s")
-            print("s directory not found, creating one")
-        except:
-            pass
-        try:
-            os.mkdir("./set1/space")
-            print("space directory not found, creating one")
-        except:
-            pass
-
-    def w():
-        capture_screen(f"./set1/w/{uuid.uuid1().hex}.png", cord=(0, 155, 1920, 1080))
-
-    def s():
-        capture_screen(f"./set1/s/{uuid.uuid1().hex}.png", cord=(0, 155, 1920, 1080))
-
-    def space():
-        capture_screen(f"./set1/space/{uuid.uuid1().hex}.png", cord=(0, 155, 1920, 1080))
-
-    print("Starting the capture")
-    keyboard.add_hotkey('w', lambda: w())
-    keyboard.add_hotkey('s', lambda: s())
-    keyboard.add_hotkey('space', lambda: space())
-    keyboard.wait()
-# capture_set1()
-
-# Function that takes care of capturing the frames for left or right turns
 def capture_set2():
+    ''' Function that takes care of capturing the frames for left or right turns '''
 
     if not (os.path.isdir("./set2") and os.path.isdir("./set1/a") and os.path.isdir("./set1/d")):
         
@@ -113,10 +140,12 @@ def capture_set2():
     keyboard.wait()
 # capture_set2()
 
-# Function that takes care of capturing the frames for nitro
 def capture_set3():
     
-    # capture Nitro and No nitro seperately two different times
+    '''
+    Function that takes care of capturing the frames for nitro
+    Capture Nitro and No nitro seperately two different times
+    '''
 
     if not os.path.isdir("./set3"):
         os.mkdir("./set3")
@@ -135,13 +164,6 @@ def capture_set3():
     keyboard.wait()
     No20()
     '''
-
-
-
-
-
-
-
 
 '''
 from ctypes import windll, Structure, c_long, byref
