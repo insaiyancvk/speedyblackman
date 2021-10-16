c22703d4915e74a7ca3caa6dcc96267c96295914
insaiyancvk <cvamshik1@gmail.com>
2021-10-16 12:52:31 +0530

    crop all frames

---
+++ cropper.py
@@ -0,0 +1,44 @@
+from PIL import Image
+import os, json, telegram
+
+def cropper(path):
+  im = Image.open(path)
+  im = im.crop((0,0,1920,920))
+  im.save(path)
+  del im
+
+paths = {
+    'a':[],
+    'd':[]
+    }
+
+paths['a'] += os.listdir('./data/set2/a')
+paths['d'] += os.listdir('./data/set2/d/')
+
+base_path_a = './data/set2/a/'
+base_path_d = './data/set2/d/'
+
+class send(): 
+
+  def __init__(self):
+    f = open ('D:/Coding/tel.json', 'r')
+    self.tel = json.load(f)
+    f.close()
+    self.bot = telegram.Bot(token=self.tel['token']) 
+
+  def msg(self, mesg):
+    self.bot.sendMessage(chat_id=self.tel['nnboiid'], text=mesg)
+
+  def pic(self, path):
+    self.bot.sendPhoto(chat_id=self.tel['nnboiid'], photo=open(path, 'rb'))
+
+bot = send()
+
+for value in paths['a']:
+  print(f'cropping {value}')
+  cropper(base_path_a+value)
+bot.msg('Completed cropping in a')
+for value in paths['d']:
+  print(f'cropping {value}')
+  cropper(base_path_d+value)
+bot.msg('Completed cropping in d')
