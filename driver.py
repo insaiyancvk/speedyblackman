24b54e04c3a2a73e9f862fc71262c75664681b48
insaiyancvk <cvamshik1@gmail.com>
2021-10-26 22:31:21 +0530

    steps for driver.py

---
+++ driver.py
@@ -0,0 +1,9 @@
+# TODO: Implement SpeedyNet
+''' 
+Steps: 
+    1. Import stuff
+    2. Take a screenshot for 'left': 0, 'top': 155, 'width': 1920, 'height': 1080
+    3. Pass the screenshot to the model
+    4. Hold down acceleration and release for 2 secs after every 5 secs
+    5. Left/Right turn only if the value is >0.65
+'''
