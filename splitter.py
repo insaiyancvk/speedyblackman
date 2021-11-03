d9cb9b6034cca198d3900c76b6e59b5f3b370ed2
insaiyancvk <cvamshik1@gmail.com>
2021-11-03 10:17:54 +0530

    seperate speedy work for acc and turns

--- splitter.py
+++ splitter.py
@@ -1,14 +1,18 @@
 # TODO: Cut down train test split to 50:50
 # Process: move 37.5% of train data from both classes to test data
-import json, shutil, os
+import shutil, os
+from math import floor
 
 SPLIT_FOLDERS = {
-    'TRAIN': os.path.join("data","set2","train"),
-    'TRAIN_a': os.path.join("data","set2","train","a"),
-    'TRAIN_d': os.path.join("data","set2","train","d"),
-    'TEST': os.path.join("data","set2","test"),
-    'TEST_a': os.path.join("data","set2","test","a"),
-    'TEST_d': os.path.join("data","set2","test","d")
+    'TRAIN': os.path.join("data","set1","train"),
+    'Extra_w': os.path.join("data","set1","extra_w"),
+    'TRAIN_w': os.path.join("data","set1","train","w"),
+    'TRAIN_s': os.path.join("data","set1","train","s"),
+    'TEST': os.path.join("data","set1","test"),
+    'TEST_w': os.path.join("data","set1","test","w"),
+    'TEST_s': os.path.join("data","set1","test","s"),
+    'w': os.path.join("data","set1","w"),
+    's': os.path.join("data","set1","s")
 }
 
 for key in SPLIT_FOLDERS:
@@ -16,39 +20,23 @@ for key in SPLIT_FOLDERS:
     os.mkdir(SPLIT_FOLDERS[key])
     print(f"{SPLIT_FOLDERS[key]} created")
 
-f = open ('data/set2/data.json', 'r')
-load_data = json.load(f)
-f.close()
+w = os.listdir(SPLIT_FOLDERS['w'])
+s = os.listdir(SPLIT_FOLDERS['s'])
 
-try:
-  print("Creating train split for 'a' class")
-  for path in load_data['train_a']:
-    shutil.move("data/"+path, SPLIT_FOLDERS['TRAIN_a'])
-except:
-  pass
-try:  
-  print("Creating train split for 'd' class")
-  for path in load_data['train_d']:
-    shutil.move("data/"+path, SPLIT_FOLDERS['TRAIN_d'])
-except:
-  pass
-try:
-  print("Creating test split for 'a' class")
-  for path in load_data['test_a']:
-    shutil.move("data/"+path, SPLIT_FOLDERS['TEST_a'])
-except:
-  pass
-try:
-  print("Creating test split for 'd' class")
-  for path in load_data['test_d']:
-    shutil.move("data/"+path, SPLIT_FOLDERS['TEST_d'])
-except:
-  pass
-try:
-  os.rmdir('set2/a')
-except:
-  pass
-try:
-  os.rmdir('set2/d')
-except:
-  pass
+dalen = len(w)
+
+for path in w[floor(0.8*dalen):]:
+  print(f"Moving {SPLIT_FOLDERS['w']}/{path}")
+  shutil.move(f"{SPLIT_FOLDERS['w']}/{path}",SPLIT_FOLDERS['TRAIN_w'])
+w = os.listdir(SPLIT_FOLDERS['w'])
+for path in w:
+  print(f"Moving {SPLIT_FOLDERS['w']}/{path}")
+  shutil.move(f"{SPLIT_FOLDERS['w']}/{path}",SPLIT_FOLDERS['TEST_w'])
+
+for path in s[floor(0.8*dalen):]:
+  print(f"Moving {SPLIT_FOLDERS['s']}/{path}")
+  shutil.move(f"{SPLIT_FOLDERS['s']}/{path}",SPLIT_FOLDERS['TRAIN_s'])
+s = os.listdir(SPLIT_FOLDERS['s'])
+for path in s:
+  print(f"Moving {SPLIT_FOLDERS['s']}/{path}")
+  shutil.move(f"{SPLIT_FOLDERS['s']}/{path}",SPLIT_FOLDERS['TEST_s'])
